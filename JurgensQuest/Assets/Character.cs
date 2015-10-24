1c6df31b61941dfdae1705c8defeb0cdbdcd0a7f
Ryxali <ryxali@gmail.com>
2015-10-24 13:08:12 +0200

    Added Character

---
+++ JurgensQuest/Assets/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+    private Rigidbody2D rBody;
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Grav"))
+        {
+            rBody.gravityScale *= 2;
+        }
+        if (Input.GetButtonUp("Grav"))
+        {
+            rBody.gravityScale /= 2;
+        }
+	}
+}
---
+++ JurgensQuest/Assets/Follower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+
+    public Transform target;
+
+    void LateUpdate()
+    {
+        transform.position = target.position;
+    }
+}
