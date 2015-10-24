6ff2015d826e3b1b1e664ab600494f2144b03fc6
Ryxali <ryxali@gmail.com>
2015-10-24 13:08:26 +0200

    Tracks now rendered with linerenderer to be visible in play

--- JurgensQuest/Assets/TrackGenerator.cs
+++ JurgensQuest/Assets/TrackGenerator.cs
@@ -7,15 +7,27 @@ public class TrackGenerator : MonoBehaviour {
     private List<Segment> list;
     private EdgeCollider2D collider;
 
+    private LineRenderer lineRender;
+
 	void Start () {
         collider = GetComponent<EdgeCollider2D>();
+        lineRender = GetComponent<LineRenderer>();
+#if UNITY_EDITOR
+        
+        lineRender.enabled = true;
+#else 
+        lineRender.enabled = false;
+#endif
         InitializeNodes();
 	}
 
 	void Update () {
 #if UNITY_EDITOR // So that we can see our nodes and path
-        collider = GetComponent<EdgeCollider2D>();
-        InitializeNodes();
+        if (!Application.isPlaying)
+        {
+            collider = GetComponent<EdgeCollider2D>();
+            InitializeNodes();
+        }
         Transform last = transform;
         foreach (Segment s in list)
         {
@@ -46,6 +58,11 @@ public class TrackGenerator : MonoBehaviour {
             p[i] = new Vector2(s.transform.localPosition.x, s.transform.localPosition.y);
         }
         collider.points = p;
+        lineRender.SetVertexCount(p.Length);
+        for (int j = 0; j < p.Length; j++)
+        {
+            lineRender.SetPosition(j, new Vector3(p[j].x, p[j].y));
+        }
                 
     }
 }
