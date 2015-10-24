f2b9d6ff17aff583add0ec16cd5805bcb45279f4
Ryxali <ryxali@gmail.com>
2015-10-24 16:37:38 +0200

    Follower now ultra cooler

--- JurgensQuest/Assets/Follower.cs
+++ JurgensQuest/Assets/Follower.cs
@@ -8,6 +8,7 @@ public class Follower : MonoBehaviour {
     public AnimationCurve angleVelocityCurve;
     public float lowestVelocityOffset = -10.0f;
     public float highestVelocityOffset = 50.0f;
+    private float speedOffset = 0.0f;
     Vector3 lastPos;
     void LateUpdate()
     {
@@ -15,16 +16,16 @@ public class Follower : MonoBehaviour {
        
         float velocity = (target.position - lastPos).magnitude / Time.deltaTime;
         float f = Mathf.InverseLerp(0.0f, maxSpeed, Mathf.Clamp(velocity, 0.0f, maxSpeed));
-
-        Debug.Log(velocity);
-        transform.position -= Vector3.right * Mathf.Lerp(lowestVelocityOffset, highestVelocityOffset, f);
+        speedOffset = Mathf.MoveTowards(speedOffset, Mathf.Lerp(lowestVelocityOffset, highestVelocityOffset, f), Time.deltaTime);
+        
         lastPos = target.position;
         RaycastHit2D rc = Physics2D.Raycast(transform.position, Vector2.down, Mathf.Infinity, LayerMask.GetMask("Floor"));
         if(rc != null) {
             Vector3 t = transform.position;
-            t.z = -(rc.distance + 1);
+            t.z = -(rc.distance + 1) / 2;
             transform.position = t;
         }
+        transform.position -= Vector3.right * speedOffset * -transform.position.z;
         transform.LookAt(target, Vector3.up);
     }
 }
