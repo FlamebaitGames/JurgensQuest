7dd01c5c6edff20a1f12eb4f8af7897f1b878d50
ViktorZryd <viktorzryd@gmail.com>
2015-10-25 20:13:57 +0100

    Uploading all the assets and the managings of music
    Dank shit yo /Gustav

---
+++ JurgensQuest/Assets/MusicManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+	public AudioSource musicLoop;
+	public AudioSource musicInit;
+
+	// Use this for initialization
+	void Start () {
+
+		musicInit.Play ();
+		musicLoop.Play ((ulong)musicInit.clip.samples);
+		//audio.PlayScheduled (musicInit.length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if (!audio.isPlaying){
+			audio.clip = MusicLoop;
+			audio.Play();
+			audio.loop = true;
+			audio.
+		}*/
+	}
+
+	/*private IEnumerator PlayList() {
+		audio.Play ();
+		yield return new WaitForSeconds (audio.clip.length);
+
+		while (true) {
+			audio.clip = musicLoop;
+			audio.Play();
+			yield return new WaitForSeconds(audio.clip.length);
+
+		}
+	}*/
+}
