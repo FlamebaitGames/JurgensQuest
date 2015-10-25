5ff8ec9f27b9c0cb2cf8d99a17a359c9038b2a7b
Ryxali <Ryxali@gmail.com>
2015-10-25 22:35:24 +0100

    Finkalibrerat

--- JurgensQuest/Assets/Timer.cs
+++ JurgensQuest/Assets/Timer.cs
@@ -15,7 +15,7 @@ public class Timer : MonoBehaviour {
         if (!paused)
         {
             elapsed = Time.time - startAt;
-            text.text = elapsed.ToString();
+			text.text = Mathf.Floor(elapsed).ToString();
         }
 	}
 
